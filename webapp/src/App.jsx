e0528e7f3819e31dffb7dd6bb7230b81571df064
Rafael Nascimento <rm553117@fiap.com.br~>
2024-04-24 06:00:29 -0300

    adicionando busca por input placeholder json

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,16 +1,46 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-
+  const [data,setData] = useState([])
+  const [records,setRecords] = useState([]) 
+   useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(res=>{
+          setData(res.data)
+          setRecords(res.data)
+        })
+    .catch(err => console.log(err))
+  },[])
+  const Filter = (e) =>{
+    setRecords(data.filter(f => f.name.toLowerCase().includes(e.target.value)))
+  }
   return (
     <>
-    <div className="title">
-      <h1>Valorant Wishlist dagalera</h1>
-    </div>
-    <div className="main--container">
-      
-      <div className="content--container"></div>
+    <div>
+      <div>
+        <input type="text" className='form-control' onChange={Filter} placeholder='Search' />
+        <table className='table'>
+          <thead>
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {records.map((d,i) =>(
+              <tr key = {i}>
+                <td>{d.id}</td>
+                <td>{d.name}</td>
+                <td>{d.email}</td>
+                <td>{d.phone}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
     </>
   )
