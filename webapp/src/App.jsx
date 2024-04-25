9ef0690a878d23e4ed49e3146e92bda682cf9dd1
Rafael Nascimento <rm553117@fiap.com.br~>
2024-04-25 11:32:46 -0300

    integrando api valorant

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -6,36 +6,35 @@ function App() {
   const [data,setData] = useState([])
   const [records,setRecords] = useState([]) 
    useEffect(()=>{
-    axios.get('https://jsonplaceholder.typicode.com/users')
+    axios.get('https://valorant-api.com/v1/weapons/skins')
     .then(res=>{
-          setData(res.data)
-          setRecords(res.data)
+          setData(res.data.data)
+          setRecords(res.data.data)
         })
     .catch(err => console.log(err))
   },[])
   const Filter = (e) =>{
-    setRecords(data.filter(f => f.name.toLowerCase().includes(e.target.value)))
+    setRecords(data.filter(f => f.displayName.toLowerCase().includes(e.target.value)))
   }
-  return (
+  console.log(records)
+  console.log()
+    return (
     <>
     <div>
       <div>
-        <input type="text" className='form-control' onChange={Filter} placeholder='Search' />
+        <input type="text" className='form-control' onChange={Filter} placeholder='Busque a Skin pelo nome da Arma ou do Bundle' />
         <table className='table'>
           <thead>
             <tr>
-              <th></th>
-              <th></th>
-              <th></th>
+              <th>Nome da Arma</th>
+              <th>Imagem da Arma</th>
             </tr>
           </thead>
           <tbody>
-            {records.map((d,i) =>(
-              <tr key = {i}>
-                <td>{d.id}</td>
-                <td>{d.name}</td>
-                <td>{d.email}</td>
-                <td>{d.phone}</td>
+            {records.map((d) =>(
+              <tr key = {d.uuid}>
+                <td>{d.displayName}</td>
+                <td><img src={d.displayIcon} alt="Imagem da arma"/></td>
               </tr>
             ))}
           </tbody>
