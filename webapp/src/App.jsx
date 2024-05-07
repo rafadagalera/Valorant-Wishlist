6f0d3623f04ae25bd84fd2e723ab948f05c272f5
dagalera <dagalera.dev@gmail.com>
2024-05-07 08:54:58 -0300

    adicionando função wishlist

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -3,44 +3,66 @@ import './App.css'
 import axios from 'axios'
 
 function App() {
-  const [data,setData] = useState([])
-  const [records,setRecords] = useState([]) 
-   useEffect(()=>{
+  const [data, setData] = useState([])
+  const [records, setRecords] = useState([])
+  const [wishlist, setWishlist] = useState([])
+
+  useEffect(() => {
     axios.get('https://valorant-api.com/v1/weapons/skins')
-    .then(res=>{
-          setData(res.data.data)
-          setRecords(res.data.data)
-        })
-    .catch(err => console.log(err))
-  },[])
-  const Filter = (e) =>{
+      .then(res => {
+        setData(res.data.data)
+        setRecords(res.data.data)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+  const Filter = (e) => {
     setRecords(data.filter(f => f.displayName.toLowerCase().includes(e.target.value)))
   }
-  console.log(records)
-  console.log()
-    return (
+
+  const addToWishlist = (item) => {
+    // Check if the item already exists in the wishlist
+    if (!wishlist.find(wishlistItem => wishlistItem.uuid === item.uuid)) {
+      setWishlist([...wishlist, item])
+    } else {
+      alert("Item is already in the wishlist!")
+    }
+  }
+
+  return (
     <>
-    <div>
       <div>
-        <input type="text" className='form-control' onChange={Filter} placeholder='Busque a Skin pelo nome da Arma ou do Bundle' />
-        <table className='table'>
-          <thead>
-            <tr>
-              <th>Nome da Arma</th>
-              <th>Imagem da Arma</th>
-            </tr>
-          </thead>
-          <tbody>
-            {records.map((d) =>(
-              <tr key = {d.uuid}>
-                <td>{d.displayName}</td>
-                <td><img src={d.displayIcon} alt="Imagem da arma"/></td>
+        <div>
+          <input type="text" className='form-control' onChange={Filter} placeholder='Search for skins by weapon name or bundle name' />
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>Weapon Name</th>
+                <th>Weapon Image</th>
+                <th>Add to Wishlist</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {records.map((d) => (
+                <tr key={d.uuid}>
+                  <td>{d.displayName}</td>
+                  <td><img src={d.displayIcon} alt="Weapon Image" /></td>
+                  <td><button onClick={() => addToWishlist(d)}>Add to Wishlist</button></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+
+      <div>
+        <h2>Wishlist</h2>
+        <ul>
+          {wishlist.map((item, i) => (
+            <li key={i}>{item.displayName}</li>
+          ))}
+        </ul>
+      </div>
     </>
   )
 }
